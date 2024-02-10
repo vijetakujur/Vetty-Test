e69743a7d224f4fae0e386fa312f89838f870e97
vijetakujur <vijetakujur05@gmail.com>
2024-02-10 13:10:59 +0530

    application can read content of given file_file4.txt

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # Read the content of file4.txt using utf-16-le encoding
+    with open("files/file4.txt", "r", encoding="utf-16-le") as file:
+        file_content = file.read()
+
+    # Pass the file content to the HTML template
+    return render_template("index.html", content=file_content)
 
 
 if __name__ == "__main__":
